5641b1da0ccfdc4b901180df4b6e9415c691cf93
Joseph Benden <joe@thrallingpenguin.com>
2007-02-14 18:22:55 +0000

    Added NULL timezone test. Patched code to set the right timezone if NULL is passed, per Bug #7748. Solaris users must also symlink /etc/localtime to correct location in /usr/share/lib/zoneinfo

--- stdtime/test.c
+++ stdtime/test.c
@@ -8,14 +8,14 @@
 int main(int argc, char **argv) {
 	struct timeval tv;
 	struct tm	tm;
-	char	*zone[4] = { "America/New_York", "America/Chicago", "America/Denver", "America/Los_Angeles" };
+	char	*zone[5] = { NULL, "America/New_York", "America/Chicago", "America/Denver", "America/Los_Angeles" };
 	int	i;
 
 	gettimeofday(&tv,NULL);
 
-	for (i=0;i<4;i++) {
+	for (i=0;i<5;i++) {
 		ast_localtime(&tv.tv_sec,&tm,zone[i]);
-		printf("Localtime at %s is %04d/%02d/%02d %02d:%02d:%02d\n",zone[i],tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+		printf("Localtime at %s is %04d/%02d/%02d %02d:%02d:%02d\n",(zone[i] == NULL ? "NULL" : zone[i]),tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	}
 	return 0;
 }
