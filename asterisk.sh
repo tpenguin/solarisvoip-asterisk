61b5d59f774ba22ee02f3a0652011a9ef83940f7
Joseph Benden <joe@thrallingpenguin.com>
2007-10-03 16:22:41 +0000

    Add a few checks to ensure Asterisk really starts, otherwise fail the SMF service.

--- asterisk.sh
+++ asterisk.sh
@@ -1,24 +1,43 @@
 #!/bin/sh
 #
-
-#
 # Start/stop asterisk
 #
 
-case "$1" in
+if [ -f /lib/svc/share/smf_include.sh ]; then
+	. /lib/svc/share/smf_include.sh
+fi
 
+if [ ! -x /opt/asterisk/usr/sbin/asterisk ]; then
+	echo "/opt/asterisk/usr/sbin/asterisk is missing or not executable."
+	exit $SMF_EXIT_ERR_CONFIG
+fi
+
+start_asterisk()
+{
+	/opt/asterisk/usr/sbin/asterisk -gnp
+	ASTPID=`cat /var/opt/asterisk/run/asterisk.pid`
+	sleep 5
+	ps -p $ASTPID >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		logger -p daemon.notice "Could not start Asterisk. Check Asterisk log files."
+		exit $SMF_EXIT_ERR_CONFIG
+	fi
+	logger -p daemon.notice "Asterisk started as PID $ASTPID."
+}
+
+case "$1" in
 'start')
-	echo "Starting Asterisk server"
-         /opt/asterisk/usr/sbin/asterisk -gnp
-        ;;
+		start_asterisk
+		;;
+
 'stop')
-         /opt/asterisk/usr/sbin/asterisk -rx "stop now"       # kill daemon
-	sleep 2
+		/opt/asterisk/usr/sbin/asterisk -rx "stop now"       # kill daemon
+		sleep 2
         ;;
 'restart')
-        /opt/asterisk/usr/sbin/asterisk -rx "stop now"
-	sleep 2
-	 /opt/asterisk/usr/sbin/asterisk -gnp
+		/opt/asterisk/usr/sbin/asterisk -rx "stop now"
+		sleep 2
+		start_asterisk
         ;;
 *)
         echo "Usage: /etc/init.d/asterisk { start | stop | restart }"
